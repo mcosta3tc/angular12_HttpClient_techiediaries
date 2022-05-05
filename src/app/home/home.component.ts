73e43d928b8d875c36fed63f34528b0b1c0b98da
msagres <m_costa3@etu-webschoolfactory.fr>
2022-05-05 17:13:04 +0200

    fix httpClient parsing error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  products = [];
 
-  constructor() { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.dataService.sendGetRequest().subscribe((data: any) => {
+      this.products = data;
+    });
   }
-
 }
